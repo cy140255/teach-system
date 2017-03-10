f3847b8308504a5f631272e7309e0c4d7c8e62f7
chenyong <chenyong@eshippinggateway.com>
2017-03-10 12:28:16 +0800

    fix

--- src/main/java/com/teach/Service/serice.java
+++ src/main/java/com/teach/Service/serice.java
@@ -1,9 +1,9 @@
-package com.teach.Controller;
+package com.teach.Service;
 
 /**
  * Created by superfq on 2017/3/10.
  */
-public class maincontroller {
+public class serice {
     public String index() {
         return "index";
     }
