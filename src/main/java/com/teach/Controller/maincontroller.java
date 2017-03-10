b28ace561455bfc5b94c2775e77e78b06bc0ceda
chenyong <chenyong@eshippinggateway.com>
2017-03-10 16:36:53 +0800

    fix

--- src/main/java/com/teach/Controller/maincontroller.java
+++ src/main/java/com/teach/Controller/maincontroller.java
@@ -1,10 +1,23 @@
 package com.teach.Controller;
 
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.*;
+import javax.ws.rs.QueryParam;
+
 /**
  * Created by superfq on 2017/3/10.
  */
+@Controller
 public class maincontroller {
-    public String index() {
+    @RequestMapping(value = "/login.do",method = RequestMethod.POST)
+    public String index(@QueryParam("username")String username,@QueryParam("password")String  password) {
+       System.out.print(username);
+        System.out.print(password);
         return "index";
     }
 }
