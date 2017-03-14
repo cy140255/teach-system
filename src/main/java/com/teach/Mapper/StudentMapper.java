acb94c79fa41710f6bb90759d92f1aed780c1115
chenyong <chenyong@eshippinggateway.com>
2017-03-14 16:00:30 +0800

    fix

--- src/main/java/com/teach/Mapper/StudentMapper.java
+++ src/main/java/com/teach/Mapper/StudentMapper.java
@@ -1,9 +1,6 @@
 package com.teach.Mapper;
 
-import com.teach.pojo.Student;
 import org.apache.ibatis.annotations.Param;
-
-import javax.ws.rs.QueryParam;
 
 /**
  * Created by superfq on 2017/3/13.
